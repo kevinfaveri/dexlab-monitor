2ca160dbb14f83702bdacded095475bbfd456496
Kevin Faveri <kevin@faveri.dev>
2021-10-31 17:16:14 -0300

    fix: add use shitcoins

--- hooks/useShitcoins.js
+++ hooks/useShitcoins.js
@@ -1,42 +1,41 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 import useLocalStorage from './useLocalStorage'
 
 export default function useShitcoins() {
-  const [volumeBelow, setVolumeBelow] = useLocalStorage("volumeBelow", 1000000);
+  const [volumeBelow, setVolumeBelow] = useLocalStorage("volumeBelow", 100000);
   const { data, mutate, isValidating } = useSWR('https://api.dexlab.space/v1/analytics/markets', axios, {
     refreshInterval: 15000
   })
   const [initialTokenList, setInitialTokenList] = useLocalStorage("initialTokenList", null);
   const [newTokenList, setNewTokenList] = useLocalStorage("newTokenList", null);
-  const [shitCoinRocketList, setShitcoinRocketList] = useLocalStorage("shitcoinRocketList", []);
+  const [shitCoinRocketList, setShitcoinRocketList] = useState([]);
 
   useEffect(() => {
     let newData = data?.data?.data
-    newData = newData?.filter((token) => token.pair.includes('/USDC'))
+    newData = newData?.filter((token) => token.pair.includes('/USDC') || token.pair.includes('/USDT'))
     const initialTokenListFailProof = (initialTokenList || [])
     let shitCoinRocketListAux = [...shitCoinRocketList]
     if (newData) {
       for (let index = 0; index < initialTokenListFailProof.length; index++) {
         const currentToken = initialTokenListFailProof[index];
         const newVolume = newData.find((token) => token.pair === currentToken.pair).todayVolume
-        if (volumeBelow > Number(newVolume)) {
+
+        if (Number(volumeBelow) > Number(newVolume) && Number(newVolume) > Number(currentToken.todayVolume)) {
+
           const tokenVolObj = {
             pair: currentToken.pair,
             oldVolume: currentToken.todayVolume,
             newVolume
           }
           const foundShitcoin = shitCoinRocketListAux.findIndex((token) => token.pair === currentToken.pair)
-          if (foundShitcoin) shitCoinRocketListAux[foundShitcoin] = tokenVolObj
-          else {
-            shitCoinRocketListAux.push(tokenVolObj)
-          }
+          if (foundShitcoin !== -1) shitCoinRocketListAux[foundShitcoin] = tokenVolObj
+          else shitCoinRocketListAux.push(tokenVolObj)
         }
       }
     }
 
-    shitCoinRocketListAux = shitCoinRocketListAux.filter((token) => token.oldVolume !== token.newVolume && Number(token.newVolume) > Number(token.oldVolume))
     shitCoinRocketListAux?.sort((a, b) => Number(b.newVolume) - Number(a.newVolume))
     setShitcoinRocketList(shitCoinRocketListAux)
 
@@ -50,7 +49,7 @@ export default function useShitcoins() {
       newTokenList?.sort((a, b) => Number(b.todayVolume) - Number(a.todayVolume))
       setNewTokenList(newTokenList)
     }
-  }, [data])
+  }, [data, volumeBelow])
 
   const removeFromNewListAddToInitialList = (pair) => {
     const pairFound = newTokenList.find((token) => token.pair === pair)
